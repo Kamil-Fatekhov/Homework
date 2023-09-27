b2a13a7a00c40f42d2641d9d262c2820d10827f8
unknown <ITMM-230042@Q113.local>
2023-09-27 11:03:05 +0300

    27.09.2023

--- FatehovKG/Practice0/Task6.09.2023/header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <stdlib.h>
-#include <math.h>
-#include <array>
-using namespace std;
-float* create_array(int n);
-float* array_sqr(float* array, int n);
-void array_out(float* array, int n);
---
+++ FatehovKG/Practice1/Vektor/Practice1/main.cpp
@@ -0,0 +1,11 @@
+#include "Header.h"
+int main() {
+	Vector i(1, 1, 1);
+	double len = i.vector_length();
+	cout << len << endl;
+	Vector j(-1, 2, -2);
+	i = i + j;
+	len = i.vector_length();
+	cout << len;
+	return 0;
+}
